f0e560f75817914c088522ec446d9c7d46d6a61b
yxs4yt <yxs4yt@virginia.edu>
2025-04-14 20:51:14 -0400

    Update bootstrapping.sh

--- mywork/bootstrapping.sh
+++ mywork/bootstrapping.sh
@@ -1,1 +1,9 @@
+##!/bin/bash
 
+/usr/bin/apt update -y
+/usr/bin/apt upgrade -y
+
+/usr/bin/apt install jq git python3-pip python3-dev -y 
+
+
+# checked with Terminal to make sure it was successful and terminated the instance
